943285e7ac87e03023bc3f9de2b7cf76d71403d4
Sahil Gautam <sahil.gautam@collabora.com>
2025-09-06 03:45:58 +0530

    transparent and dark, like it this way

--- src/x.c
+++ src/x.c
@@ -162,7 +162,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha             = 1.0;
+float alpha             = 0.6;
 const char *colorname[] = {
 
     /* 8 normal colors */
@@ -185,8 +185,8 @@ const char *colorname[] = {
     [15] = "#d8dee9", /* white   */
 
     /* special colors */
-    [256] = "#282c34", /* background */
-    [257] = "#bbc2cf", /* foreground */
+    [256] = "#000104", /* background */
+    [257] = "#d8dee9", /* foreground */
     [258] = "#51afef", /* cursor */
 };
 
